c1f6f444201148d779aa7067838e4d56dbfdd7fb
Chris Diego <christopher.diego@coxautoinc.com>
2018-12-08 13:45:44 -0500

    building index page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-
 import AppRouter from './AppRouter';
 
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#00897b',
-    },
-    secondary: {
-      main: '#618cb9',
+      main: '#78909c',
+      light: '#a7c0cd',
+      dark: '#4b636e',
     },
   },
 });
