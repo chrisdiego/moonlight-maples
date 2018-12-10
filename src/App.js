ab521adfd18ea77458ace3d014d6043a2b8f9189
Chris Diego <christopher.diego@coxautoinc.com>
2018-12-09 23:03:17 -0500

    rebooting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import AppRouter from './AppRouter';
+import React, { Component } from 'react';
+import Header from './Header';
+import IndexPage from './IndexPage';
+import './App.css';
 
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#78909c',
-      light: '#a7c0cd',
-      dark: '#4b636e',
-    },
-  },
-});
-
-
-function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <AppRouter />
-    </MuiThemeProvider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <IndexPage />
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
