15245a2a2fd04464f83cdf59ea572f254d5f229c
Chris Diego <christopher.diego@coxautoinc.com>
2018-12-10 21:47:37 -0500

    keep on truckin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import IndexPage from './IndexPage';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App bg-light">
         <Header />
         <IndexPage />
       </div>
